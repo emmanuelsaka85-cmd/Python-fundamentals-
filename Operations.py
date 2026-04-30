34d5afa811c931b9dd3159e3bad7f1578c5d6409
Saka Emmanuel Jonathan <emmanuelsaka85@gmail.com>
2026-05-01 00:47:44 +0300

    Implement basic arithmetic operations in Operations.py
    Add operations demonstrating order of operations and modulo.

---
+++ Operations.py
@@ -0,0 +1,7 @@
+mixedNumbers = 1 +2 * 3 / 4.0
+
+# Try to predict what the answer will be ... does Python follow order operations hierarchy?
+print(mixedNumbers)
+# The modulo (%) returns the integer remainder of a division
+remainder = 11 % 3
+print(remainder)
